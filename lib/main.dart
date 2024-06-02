3f0e4e39188b88ca39de281a9e73e7f391428c0b
akash yanakandla <akashyanakandla0073@gmail.com>
2024-06-02 12:10:14 +0530

    this is the third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app_trial2/start_screen.dart';
+import 'package:quiz_app_trial2/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.purple,
-                Colors.deepPurple,
-              ],
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_trial2/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.purple,
+                Colors.deepPurple,
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
